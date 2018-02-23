2ca6e1d09e989e879ede171a258ff57644bf285f
eyasin18 <eyasin18@gmail.com>
2018-02-23 23:53:06 +0100

    Update 15

--- fingerhut/src/de/repictures/fingerhut/Backend/PrivateLogin.java
+++ fingerhut/src/de/repictures/fingerhut/Backend/PrivateLogin.java
@@ -25,7 +25,7 @@ import java.util.logging.Logger;
 public class PrivateLogin extends HttpServlet {
 
     private Logger log = Logger.getLogger(Account.class.getName());
-    public static final int appVersion = 14;
+    public static final int appVersion = 15;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
