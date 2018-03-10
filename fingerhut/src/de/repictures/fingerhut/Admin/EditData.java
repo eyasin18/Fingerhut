0f075dda8b72799b540984835963cff991db25e7
Maxnet <TEEMA>
2018-03-10 22:45:35 +0100

    Gerhardt

--- fingerhut/src/de/repictures/fingerhut/Admin/EditData.java
+++ fingerhut/src/de/repictures/fingerhut/Admin/EditData.java
@@ -1,15 +1,20 @@
 package de.repictures.fingerhut.Admin;
 
 import javax.servlet.ServletException;
+import com.google.appengine.api.datastore.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class EditData extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        com.google.appengine.api.datastore.Query query = new com.google.appengine.api.datastore.Query("PurchaseOrder");
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        List<Entity> entities = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+        resp.getWriter().println(entities.size());
     }
 }
